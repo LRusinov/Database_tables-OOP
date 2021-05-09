dbdd69004cb3a7a4b2aec8bc66982b529865377c
LRusinov <80131778+LRusinov@users.noreply.github.com>
2021-05-09 06:01:58 +0300

    Update Table.h

--- Table.h
+++ Table.h
@@ -1,9 +1,10 @@
 #include "Column.h"
 class Table {
 private:
-	MyString name;
+	std::string name;
 	size_t number_of_colums;
 	Column* columns;
 public:
 	void print();
+	void Read_from_file();
 };
