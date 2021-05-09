edba618fc9decf0b9b1a842b4547cbbcba229fd2
LRusinov <80131778+LRusinov@users.noreply.github.com>
2021-05-09 06:01:19 +0300

    Update Column.h

--- Column.h
+++ Column.h
@@ -4,55 +4,19 @@ class Column {
 private:
 	type column_type;
 	size_t number_of_rows;
-	MyString* rows;
+	std::string* rows;
 public:
-	Column() {
-		column_type = type::Null;
-		number_of_rows = 0;
-		rows=nullptr;
-	}
-	Column(Column& other) {
-
-			this->column_type = other.column_type;
-			this->number_of_rows = other.number_of_rows;
-			this->rows = other.rows;
-	}
-	~Column() {
-		delete[] rows;
-	}
+	Column();
+	Column(Column& other);
+	~Column();
 
 
-	void set_columtype(type _column_type) {
-		column_type = _column_type;
-	}
-	type get_columtype() {
-		return this->column_type;
-	}
-	type type_check(MyString* _rows) {
-		if (_rows[0][0] == '"') {
-			return type::String;
-		}
-		else {
-			if (_rows[0].get_lenght() == 0) {
-				return type::Null;
-			}
-			bool is_there_a_point = false;
-
-			for (size_t i = 0; i < _rows[0].get_lenght()+1; i++) {
-				if (_rows[0][i] == '.') {
-					is_there_a_point = true;
-					break;
-				}
-			}
-			if (is_there_a_point) {
-				return type::Double;
-			}
-			else {
-				return type::Integer;
-			}
-		}
-	}
-	void Read_from_file();
+	void set_columtype(std::string _column_type);
+	type get_columtype();
+	type type_check(std::string* _rows);
+	void set_row(std::string new_row);
+	std::string get_row(size_t n);
+	
 
 	
 
