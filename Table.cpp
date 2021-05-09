297a0e73afe3f4a2bcd2c882a2426074d1fa8043
LRusinov <80131778+LRusinov@users.noreply.github.com>
2021-05-09 15:16:35 +0300

    Update Table.cpp

--- Table.cpp
+++ Table.cpp
@@ -52,17 +52,25 @@ void Table::Read_from_file() {
             counter++;
         }
         else if(text[0]=='|'){
+            
+
             column_counter = 0;
             size_t index=1,i=0;
             std::string buff;
-            buff.resize(text.length());
-            while (index < text.length() - 1) {
+            buff.resize(text.length()+1);
+            while (index < text.length()-1&& column_counter<number_of_colums) {
+                
                 while (text[index] != '|') {
+          
                     buff[i] = text[index];
+                
                     i++;
                     index++;
                 }
+                
                 columns[column_counter].set_row(buff);
+                
+                
                 column_counter++;
                 i = 0;
                 index++;
@@ -74,23 +82,28 @@ myfile.close();
 }
 void Table::print() {
     size_t counter=0;
-    for (size_t i = 0; i < number_of_colums * 21 + 1;i++) {
+    
+    for (size_t i = 0; i < number_of_colums * 19;i++) {
         std::cout << "=";
     }
     std::cout << std::endl;
-    for (size_t i = 0; i < number_of_colums ; i++) {
-        std::cout << columns[i].get_row(counter);
-        if (columns[i].get_row(counter).length() < 20) {
-            for (size_t i = 0; i < 20 - columns[i].get_row(counter).length(); i++) {
-                std::cout << " ";
+    while (counter < columns[0].get_number_of_rows()) {
+        std::cout << "|";
+        for (size_t i = 0; i < number_of_colums; i++) {
+            std::cout << columns[i].get_row(counter);
+            if (columns[i].get_row(counter).length() < 20) {
+                for (size_t g = 0; g < 20 - columns[i].get_row(counter).length(); g++) {
+                    std::cout << " ";
+                }
             }
+            std::cout << "|";
         }
-        std::cout << "|"<<std::endl;
-        counter++;
-        for (size_t i = 0; i < number_of_colums * 21 ; i++) {
+        std::cout << std::endl;
+        for (size_t i = 0; i < number_of_colums * 19; i++) {
             std::cout << "=";
         }
-        std::cout << "|" << std::endl;
+        std::cout << std::endl;
+        counter++;
     }
 
 }
