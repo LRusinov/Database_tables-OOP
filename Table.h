dff5c2a5bfc29efc36dffc9177fab0880eba1a93
LRusinov <80131778+LRusinov@users.noreply.github.com>
2021-05-11 04:12:34 +0300

    Update Table.h

--- Table.h
+++ Table.h
@@ -5,6 +5,11 @@ private:
 	size_t number_of_colums;
 	Column* columns;
 public:
+	std::string get_name()const;
 	void print();
-	void Read_from_file();
+	void Read_from_file(std::ifstream&  myfile);
+	template<typename T>
+	T add(T* arr, const size_t size_of_arr, const T element_to_add);
+	void show_columnstypes();
+	void search_row(const size_t n, const std::string value);
 };
