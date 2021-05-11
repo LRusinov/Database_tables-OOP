f4f1cdd83c1ba088aa9148d72f0661280c358011
LRusinov <80131778+LRusinov@users.noreply.github.com>
2021-05-12 02:41:05 +0300

    Update Database.h

--- Database.h
+++ Database.h
@@ -13,16 +13,22 @@ public:
 		number_of_files = 0;
 		number_of_tables = 0;
 	}
-	void import(const std::string file);
-	void showtables();
-	void describe(const std::string table_name);
-	void print(const std::string table_name);
+	Database(Database& other) {
+		this->all_tables = other.all_tables;
+		this->names_of_files = other.names_of_files;
+		this->number_of_files = other.number_of_files;
+		this->number_of_tables = other.number_of_tables;
+	}
+	void import(const std::string file);//
+	void showtables();//
+	void describe(const std::string table_name);//
+	void print(const std::string table_name);//
 	void select(const size_t column_n,const std::string value,const std::string table_name);
 	void addcolumn(std::string table_name, std::string column_type);
 	void remove(const std::string table_name, const size_t column_n,const std::string value);
-	void rename(std::string old_name, std::string new_name);
-	size_t count(const std::string table_name, const size_t column_n, const std::string value);
-	void export_table(const std::string table_name,const std::string file_name);
+	void rename(const std::string old_name,const std::string new_name);//
+	size_t count(const std::string table_name, const size_t column_n, const std::string value);//
+	void export_table(const std::string table_name,const std::string file_name);//?
 	void update(const std::string table_name, const size_t column_n, const std::string value, const size_t target_column_n, const std::string target_value);
 
 	//insert <table name> <column 1> … <column n>
