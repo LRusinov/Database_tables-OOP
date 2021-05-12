c153ca6647a17160639bc88b8cfb428c764877da
LRusinov <80131778+LRusinov@users.noreply.github.com>
2021-05-13 00:01:53 +0300

    Update Column.h

--- Column.h
+++ Column.h
@@ -9,12 +9,13 @@ public:
 	Column();
 	Column(Column& other);
 	//~Column();
-	
+	void set_number_of_rows(const size_t n);
 	size_t get_number_of_rows()const;
 	void set_columtype(const std::string _column_type);
 	type get_columtype()const;
 	type type_check(std::string* _rows);
-	void set_row(const std::string new_row);
+	void new_row(const std::string new_row);
+	void set_row(const size_t index, const std::string new_row_name);
 	std::string get_row(const size_t n)const;
 	void pr();
 	void print_row();
