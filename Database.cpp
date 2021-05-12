53e7eee76c8e086abbf8b4b88af0d39cadae4a54
LRusinov <80131778+LRusinov@users.noreply.github.com>
2021-05-13 00:02:19 +0300

    Update Database.cpp

--- Database.cpp
+++ Database.cpp
@@ -49,27 +49,85 @@ void Database::print(const std::string table_name) {
 		searched_table.print();
 }
 void Database::select(const size_t column_n,const std::string value,const std::string table_name) {
-	Table searched_table;
-	searched_table.search_table(table_name, all_tables, number_of_tables);
-	searched_table.search_rows(column_n, value);
+	std::ofstream f("x.txt");
+	size_t index = -1;
+	for (size_t i = 0; i < number_of_tables; i++) {
+		if (all_tables[i].get_name() == table_name) {
+			index = i;
+			break;
+		}
+	}
+	if (index == -1) {
+		std::cout << "There is no table with this name in the database!" << std::endl;
+	}
+	else {
+		for (size_t i = 0; i < all_tables[index].get_column(0).get_number_of_rows(); i++) {
+			f<< all_tables[index].get_column(column_n).get_row(i) << "|" << value;
+			std::cout << value.compare(all_tables[index].get_column(column_n).get_row(i));
+			if (value.compare(all_tables[index].get_column(column_n).get_row(i))==0) {
+				all_tables[index].get_column(column_n).print_row();
+			}
+
+
+		}
+	}
+	f.close();
 }
 
 void Database::addcolumn(std::string table_name, std::string column_type) {
-	Table searched_table;
-	Column new_column;
-	searched_table.search_table(table_name, all_tables, number_of_tables);
-	//searched_table.add<Column>(searched_table.get_columns(), searched_table.get_num_of_columns(), new_column);
-	searched_table.get_column(searched_table.get_num_of_columns() - 1).set_columtype(column_type);
-}
-void Database::remove(const std::string table_name, const size_t column_n, const std::string value) {
-	Table searched_table;
-	searched_table.search_table(table_name, all_tables, number_of_tables);
-	for (size_t i = 0; i < searched_table.get_num_of_columns(); i++) {
-		if (searched_table.get_column(column_n).get_row(i) == value) {
-			searched_table.get_column(column_n).delete_row(i);
+	size_t index = -1;
+	for (size_t i = 0; i < number_of_tables; i++) {
+		if (all_tables[i].get_name() == table_name) {
+			index = i;
+			break;
 		}
 	}
+	if (index == -1) {
+		std::cout << "There is no table with this name in the database!" << std::endl;
+	}
+	else {
+		Column new_column;
+		new_column.set_columtype(column_type);
+		//new_column.set_number_of_rows(all_tables[index].get_column(0).get_number_of_rows());
+		all_tables[index].set_num_of_columns(all_tables[index].get_num_of_columns() + 1);
+		for (size_t g = 0; g < all_tables[index].get_column(0).get_number_of_rows(); g++) {
+			new_column.new_row("");
+		}
+		//searched_table.add<Column>(searched_table.get_columns(), searched_table.get_num_of_columns(), new_column);
+		
+		Column* buff = new Column[all_tables[index].get_num_of_columns()];
+		for (size_t i = 0; i < all_tables[index].get_num_of_columns() - 1; i++) {
+			buff[i] = all_tables[index].get_column(i);
+		}
+		all_tables[index].destroy_columns();
+		all_tables[index].new_columns(all_tables[index].get_num_of_columns());
 
+		for (size_t i = 0; i < all_tables[index].get_num_of_columns() - 1; i++) {
+			all_tables[index].set_column(i,buff[i]);
+		}
+		delete[] buff;
+		all_tables[index].set_column(all_tables[index].get_num_of_columns() - 1, new_column);
+	}
+
+}
+void Database::remove(const std::string table_name, const size_t column_n, const std::string value) {
+	size_t index = -1;
+	for (size_t i = 0; i < number_of_tables; i++) {
+		if (all_tables[i].get_name() == table_name) {
+			index = i;
+			break;
+		}
+	}
+	if (index == -1) {
+		std::cout << "There is no table with this name in the database!" << std::endl;
+	}
+	else {
+		for (size_t i = 0; i < all_tables[index].get_num_of_columns(); i++) {
+			if (all_tables[index].get_column(column_n).get_row(i) == value) {
+				all_tables[index].get_column(column_n).delete_row(i);
+			}
+		}
+	}
 }
 void Database::rename(const std::string old_name,const std::string new_name) {
 	int index=-1;
@@ -118,15 +176,14 @@ void Database::export_table(const std::string table_name, const std::string file
 	myfile << std::endl;
 	myfile << searched_table.get_name()<<std::endl;
 	size_t counter = 0;
-	for (size_t i = 0; i < searched_table.get_num_of_columns() * 19; i++) {
+	for (size_t i = 0; i < searched_table.get_num_of_columns() * 21; i++) {
 		myfile << "=";
 	}
 	myfile << std::endl;
 	while (counter < searched_table.get_column(0).get_number_of_rows()) {
 		myfile << "|";
 		for (size_t i = 0; i < searched_table.get_num_of_columns(); i++) {
-			//std::cout << searched_table.get_column(i).get_row(counter) << "|";
-			myfile << searched_table.get_column(i).get_row(counter) << "|";
+			myfile << searched_table.get_column(i).get_row(counter) ;
 			if (searched_table.get_column(i).get_row(counter).length() < 20) {
 				for (size_t g = 0; g < 20 - searched_table.get_column(i).get_row(counter).length(); g++) {
 					myfile << " ";
@@ -135,7 +192,7 @@ void Database::export_table(const std::string table_name, const std::string file
 			myfile << "|";
 		}
 		myfile << std::endl;
-		for (size_t i = 0; i < searched_table.get_num_of_columns() * 19; i++) {
+		for (size_t i = 0; i < searched_table.get_num_of_columns() * 21; i++) {
 			myfile << "=";
 		}
 		myfile << std::endl;
@@ -156,12 +213,22 @@ size_t Database::count(const std::string table_name, const size_t column_n,const
 }
 
 void Database::update(const std::string table_name, const size_t column_n, const std::string value, const size_t target_column_n, const std::string target_value) {
-	Table searched_table;
-	searched_table.search_table(table_name, all_tables, number_of_tables);
-	for (size_t i = 0; i < searched_table.get_num_of_columns(); i++) {
-		if (searched_table.get_column(column_n).get_row(i) == value) {
-			searched_table.get_column(target_column_n).set_row(target_value);
+	size_t index = -1;
+	for (size_t i = 0; i < number_of_tables; i++) {
+		if (all_tables[i].get_name() == table_name) {
+			index = i;
 			break;
+		}
+	}
+	if (index == -1) {
+		std::cout << "There is no table with this name in the database!" << std::endl;
+	}
+	else {
+		for (size_t i = 0; i < all_tables[index].get_num_of_columns(); i++) {
+			if (all_tables[index].get_column(column_n).get_row(i) == value) {
+				//all_tables[index].get_column(target_column_n).set_row(target_value);
+				break;
+			}
 		}
 	}
 }
