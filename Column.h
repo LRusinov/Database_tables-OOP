a12b0d5d70b85b644dd941ecd1222c409d655e32
LRusinov <80131778+LRusinov@users.noreply.github.com>
2021-06-06 17:57:54 +0300

    Added comments

--- Column.h
+++ Column.h
@@ -1,30 +1,29 @@
 #include<iostream>
 #include<string>
+
 enum class type{Integer,Double,String,Unknown};
+
 class Column {
 private:
 	type column_type;
 	size_t number_of_rows;
 	std::string* rows;
 public:
+	//Constructors
 	Column();
 	Column(Column& other);
 
-	void set_row(const size_t index, const std::string& new_row_name);
-	std::string get_row(const size_t n)const;
-	void set_number_of_rows(const size_t n);
+	void set_row(const size_t& index, const std::string& new_row_name);
+	std::string get_row(const size_t& n)const;
+	void set_number_of_rows(const size_t& n);
 	size_t get_number_of_rows()const;
 	void new_row(const std::string& new_row);
-	void print_row();
-	void delete_row(const size_t n);
+	void delete_row(const size_t& n);
 
 	void set_columtype(const std::string& _column_type);
 	type get_columtype()const;
-	type type_check(std::string* _rows);
 
+	//Helper functions
 	double string_to_double(const std::string& str);
 	int string_to_int(const std::string& str);
-
-	
-
 };
